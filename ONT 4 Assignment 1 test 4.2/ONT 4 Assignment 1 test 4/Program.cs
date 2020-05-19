085151c70d29d36760eca12ce0ef8d07e7f79e31
AbrePio <50925276+AbrePio@users.noreply.github.com>
2020-05-19 22:53:33 +0200

    Add files via upload

---
+++ ONT 4 Assignment 1 test 4.2/ONT 4 Assignment 1 test 4/Program.cs
@@ -0,0 +1,69 @@
+﻿using ONT_4_Assignment_1_test_4.Fillings;
+using System;
+
+namespace ONT_4_Assignment_1_test_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            string ing, kitchen;
+            Console.Write("Enter Country (SA/US): ");
+            kitchen = Console.ReadLine();
+            Console.Write("Do you want to create a Taco or Burrito? ");
+            ing = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            if(kitchen == "SA" || kitchen == "sa")
+            {
+                if(ing == "Taco" || ing == "taco")
+                {
+                    Taco saTaco = new Taco(new SAIngredientFactory());
+                    Console.WriteLine($"SA Taco: {saTaco.DescribeTaco()}");
+                }
+                else if(ing == "Burrito" || ing == "burrito")
+                {
+                    Burrito saBurrito = new Burrito(new SAIngredientFactory());
+                    Console.WriteLine($"SA Burrito: {saBurrito.DescribeBurrito()}");
+                }
+                
+                
+            }
+            else if(kitchen == "US" || kitchen == "us")
+            {
+                if (ing == "Taco" || ing == "taco")
+                {
+                    Taco usTaco = new Taco(new USIngredientFactory());
+                    Console.WriteLine($"US Taco: {usTaco.DescribeTaco()}");
+                }else if(ing == "Burrito" || ing == "burrito")
+                {
+                    Burrito usBurrito = new Burrito(new USIngredientFactory());
+                    Console.WriteLine($"US Burrito: {usBurrito.DescribeBurrito()}");
+                }
+                
+                
+                
+            }
+
+            Ingredient yes = new Burrito(new SAIngredientFactory());
+            yes = new Chicken(yes);
+            yes = new Mutton(yes);
+            yes = new Samp(yes);
+            //yes = new Beef(yes);
+            //yes = new BlackBeans(yes);
+            Console.WriteLine(yes.GetDiscription() + " \tFinal Price: " + yes.Cost().ToString("C"));
+
+
+            
+            
+
+            
+            
+
+            Console.ReadLine();
+        }
+    }
+}
